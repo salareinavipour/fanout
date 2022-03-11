e539de3211c39367442947f23b4ac3fb26796ea6
Salar Einavi Pour <salareinavipour@gmail.com>
2022-03-11 10:29:21 +0330

    Update provider to have another essential endpoint

--- provider.tf
+++ provider.tf
@@ -7,5 +7,6 @@ provider "aws" {
     iam            = "http://localhost:4566"
     sns            = "http://localhost:4566"
     sqs            = "http://localhost:4566"
+    sts            = "http://localhost:4566"
   }
 }
